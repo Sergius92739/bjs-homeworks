6f741a8d973fa18d811acb4843235756601c8b86
Sergius92739 <sergem279@gmail.com>
2021-02-27 08:26:26 +0300

    fixed bugs

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,18 +1,16 @@
+"use strict";
 function getResult(a, b, c) {
-    //2x^2+4x-3=0
-    "use strict";
+    //2x^2+4x-3=0 
     let x = [];
     const discriminant = (b ** 2) - (4 * a * c);
     console.log(discriminant);
-    if (discriminant < 0) {
-        x === [null];
-    } else if (discriminant === 0) {
+    if (discriminant === 0) {
         let x0 = -(b) / (2 * a);
-        x === x.push(x0);
-    } else {
+        x.push(x0);
+    } else if (discriminant > 0) {
         let x1 = (- b + Math.sqrt(discriminant)) / (2 * a);
         let x2 = (- b - Math.sqrt(discriminant)) / (2 * a);
-        x === x.push(x1, x2);
+        x.push(x1, x2);
     }
     return x;
 }
@@ -20,34 +18,30 @@ function getResult(a, b, c) {
 
 function getAverageMark(marks) {
     let sum = 0;
+
     if (marks.length === 0) {
         return 0;
-    } else if (marks.length > 5) {
+    } else {
         console.log(marks);
         marks.splice(5);
         console.log(marks);
     }
+
     for (let i = 0; i < marks.length; i++) {
         sum = sum + marks[i];
     }
-    let averageMark = sum / marks.length;
 
-    return averageMark;
-
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    return sum / marks.length;
 }
 
 function askDrink(name, dateOfBirthday) {
-    let today = new Date();
-    let yearToday = today.getFullYear();
-    let yearOfBirth = dateOfBirthday.getFullYear();
-    let age = yearToday - yearOfBirth;
+  
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+
     if (age >= 18) {
         return `Не желаете ли олд-фэшн, ${name}?`;
     } else {
         return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`
     }
-    // код для задачи №3 писать здесь
-    // return result;
+    
 }
